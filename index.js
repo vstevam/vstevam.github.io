0e8b7f6e382d1ecc2a361dae74960bf17d3a0d9b
Vinicius Stevam <vinicius.stevam@consensys.net>
2019-01-28 17:42:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var typed = new Typed('.type', {
+  strings:[ "",
+            "Hi! How are U?",
+            "I'm Vinicius Stevam"],
+   typeSpeed: 130,
+   backDelay: 200
+});
+
+//showing text
+$( ".showing" ).show( 500, function() {
+  });
